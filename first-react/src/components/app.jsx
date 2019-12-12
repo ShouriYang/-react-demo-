df5eb8a412176362fc32d9acb6e7979be76058cc
ShouriYang <451044764@qq.com>
2019-12-12 19:47:18 +0800

    react-create-app 第一个小demo

---
+++ first-react/src/components/app.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg'
+import '../index.css'
+class App extends Component {
+    render() { 
+        return ( 
+            <div>
+                <img className='logo' src={logo} alt="logo"/>
+                <p className='title'>react app 组件</p>
+            </div>
+         );
+    }
+}
+ 
+export default App;
